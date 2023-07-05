6e67437e42b0a269f8701fe4983af513b2ae4b00
BitHero <nuhumusah874@gmail.com>
2023-07-05 23:00:21 +0000

    styling the app

--- learning_charts/views.py
+++ learning_charts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -6,19 +9,26 @@ def index(request):
     """ The Home page for learning charts"""
     return render(request, 'learning_charts/index.html')
 
+@login_required
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render (request, 'learning_charts/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all it's entries"""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belong to the current user
+    if topic.owner != request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_charts/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
@@ -28,12 +38,15 @@ def new_topic(request):
         # Post data is submitted, process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_charts:topics')
     # Display a blank or invalid form
     context = {'form': form}
     return render(request, 'learning_charts/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a partcular topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -52,10 +65,14 @@ def new_entry(request, topic_id):
     context = {'topic':topic, 'form': form}
     return render(request, 'learning_charts/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request, pre-fill form with the current entry
